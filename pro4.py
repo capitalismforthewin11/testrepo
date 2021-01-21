d4ccef080d7d5c8c197b65b975b6cc4ff5196f00
capitalismforthewin11 <77788930+capitalismforthewin11@users.noreply.github.com>
2021-01-21 19:31:48 +0530

    Add files via upload

---
+++ pro4.py
@@ -0,0 +1,11 @@
+a=4
+b=int(input("enter your favourite number: "))
+
+if a>b:
+    print("the greatest number is:")
+    print(a)
+elif a<b:
+    print("the greatest number is:")
+    print(b)
+else:
+    print("the numbers are equal!")
